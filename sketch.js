bd9b521ac7d2d45aa3e82f5f1319f8911a4581d8
tech2020traininggh <65932244+tech2020traininggh@users.noreply.github.com>
2020-09-22 12:52:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,12 +20,12 @@ Finally, we will be writing a condition which tells when the game will end.
 // loading  the images in our code
 
 function preload(){
-  track = loadImage("images/track.jpg");
-  car1_img = loadImage("images/car1.png");
-  car2_img = loadImage("images/car2.png");
-  car3_img = loadImage("images/car3.png");
-  car4_img = loadImage("images/car4.png");
-  ground = loadImage("images/ground.png");
+  track = loadImage("/../images/track.jpg");
+  car1_img = loadImage("/../images/car1.png");
+  car2_img = loadImage("/../images/car2.png");
+  car3_img = loadImage("/../images/car3.png");
+  car4_img = loadImage("/../images/car4.png");
+  ground = loadImage("/../images/ground.png");
 }
 
 function setup(){
